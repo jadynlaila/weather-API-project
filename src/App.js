da08fbae8fe55e4fb85b72bf788bff6aa51b524b
Estrella Angel <Eangel262@west-mec.local>
2021-05-05 15:16:14 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import DailyPage from './components/DailyPage'
 import WeeklyPage from './components/WeeklyPage'
 import Nav from './components/Nav'
+import Slider from "./components/Slider"
 
 function App() {
   return (
     <>
+    <div id="app"></div>
+    <DailyPage></DailyPage>
+
     </>
   );
 }
--- src/components/DailyPage.jsx
+++ src/components/DailyPage.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
 
 function DailyPage(props) {
+
+    function getCords(){   
+        x = event.pageX;
+        y = event.pageY;
+        console.log("(X: " + x + ", Y: " + y + ")");
+    }
+
+
     return (
         <>
         <div id="weatherAnimation" className="weatherAnimation">
         </div>
-        <div id="weatherSlider" className="weatherSlider"></div>
+        <div id="weatherSlider" className="weatherSlider"> 
+            <div className="my-5">
+                <label htmlFor="customRange1">Time of Day</label>
+                <input type="range" className="custom-range" id="customRange1" onMouseDown={() => {  }}/>
+            </div>
+        </div>
         <div id="shortWeatherInfo" className="shortWeatherInfo">
-            
+
         </div>
         </>
     )
