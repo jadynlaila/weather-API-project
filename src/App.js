0c6177d66bc4c3d1a32c5b00a8659f2f2a912f93
Estrella Angel <Eangel262@west-mec.local>
2021-05-06 14:19:36 -0700

    Adding slider with working values and sun img

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import DailyPage from './components/DailyPage'
 import WeeklyPage from './components/WeeklyPage'
 import Nav from './components/Nav'
+import SunIcon from './images/sun.png'
 
 function App() {
   return (
     <>
     <div id="app"></div>
     <DailyPage></DailyPage>
-
+    <img src={SunIcon} alt="no image"/>
     </>
   );
 }
