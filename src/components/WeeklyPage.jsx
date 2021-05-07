02aac0fe1b3d8159593128eccf55fe8af5a66c43
Jadyn Calhoun <jaycal2013@gmail.com>
2021-05-07 14:12:42 -0700

    sorting things out

--- src/components/WeeklyPage.jsx
+++ src/components/WeeklyPage.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react'
 
-const WeeklyPage = () => {
+const WeeklyPage = (weather) => {
+    console.log(weather[1]);
+
+    let weatherElements = JSON.parse(weather);
+    console.log(weatherElements);
     return (
+        <>
         <div>
-            
+            <div id='day1'>
+                {/* <p>{weather.lat}</p> */}
+            </div>
         </div>
+        </>
     )
 }
 
