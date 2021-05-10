65773c752697cd53e45cca1eddaca56070163c51
Jadyn Calhoun <jaycal2013@gmail.com>
2021-05-10 14:41:04 -0700

    pulling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import DailyPage from './components/DailyPage'
 import WeeklyPage from './components/WeeklyPage'
 import Navigation from './components/Navigation'
 import SunIcon from './images/sun.png'
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import React, { useState } from 'react'
 import weather from "./weather.json"
 
 
 
 const App = () => {
-  const [showWeekly, setshowWeekly] = useState(true);
-  const [showDaily, setshowDaily] = useState(true);
+  const [showWeekly, setShowWeekly] = useState(false);
+  const [showDaily, setShowDaily] = useState(true);
 
+  
   return (
     <>
-      <Navigation />
+      <Navigation onShowDaily={() =>{
+        setShowDaily(!showDaily);
+        setShowWeekly(!showWeekly);
+      }}
+        showDaily={showDaily}
+
+        onOpenWeekly={() => {
+          setShowWeekly(!showWeekly)
+        }}
+        showWeekly={showWeekly} />
       { showDaily && <DailyPage />}
       { showDaily && <img src={SunIcon} alt="no image" />}
-      { showWeekly && <WeeklyPage weather={weather}/>}
+      { showWeekly && <WeeklyPage weather={weather} />}
       <Button as="input" type="submit" value="Submit" />{' '}
       {/* {console.log(weather.hourly[0].dt)} */}
     </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-function Nav() {
+function Nav(openDaily, showDaily, openWeekly, showWeekly) {
     return (
         <div>
             <nav>
                 <div className="navContainer">
-                    <div className="link" id="home">Home</div>
-                    <div className="link" id="week">Weekly</div>
+                    <div className="link" id="home" onClick={openDaily}>Home</div>
+                    <div className="link" id="week" onClick={openWeekly}>Weekly</div>
                     <div className="link" id="humid">Humidity</div>
                 </div>
             </nav>
