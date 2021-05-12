bd6655d1ef8fb97d2d1763b0cd0320471678d17b
Jadyn Calhoun <jaycal2013@gmail.com>
2021-05-12 15:16:45 -0700

    working on styling

--- src/components/DailyPage.jsx
+++ src/components/DailyPage.jsx
@@ -33,47 +33,47 @@ function DailyPage({ slider, dailyInfo, sunImg, Col, Row, Container }) {
     //     //referenced all the different values by saying formattedTime[whateveryouneed]
 
     // }
-    function getGMTTime(timestamp){
-        
-        timestamp = timestamp*1000;
+    function getGMTTime(timestamp) {
+
+        timestamp = timestamp * 1000;
         setFormattedTime(new Date(timestamp).toLocaleDateString("en-US"));
         setformattedHour(new Date(timestamp).toLocaleTimeString("en-US"))
         // expected output "8/30/2017" 
-      }
+    }
 
     // function orderHrlyTime(date){
     //     if(date  )
     // }
-    
+
 
 
     return (
         <>
-        <div id="weatherAnimation" className="weatherAnimation"> 
-        </div>
-        <div id="weatherSlider" className="weatherSlider"> 
-            <div className="my-5">
-                <AiOutlineLeft className="leftDateBtn"/>
-                <label htmlFor="customRange1">Time of Day</label>
-                <AiOutlineRight className="rightDateBtn"/>
-                <input 
-                type="range" 
-                className="custom-range" 
-                id="customRange1" 
-                style={{width: "600px"}} 
-                min={0} 
-                max={24} 
-                value={range}
-                onChange={(e) => {
-                    setRange(e.target.value);
-                    console.log(e.target.value);
-                    getHourStats(e.target.value);
-                    getGMTTime(slider[e.target.value].dt)
-                }} 
-                />
+            <div id="weatherAnimation" className="weatherAnimation">
             </div>
-        </div>
-        <div id="shortWeatherInfo" className="shortWeatherInfo">
+            <div id="weatherSlider" className="weatherSlider">
+                <div className="my-5">
+                    {/* <AiOutlineLeft className="leftDateBtn"/> */}
+                    <label htmlFor="customRange1">Time of Day</label>
+                    {/* <AiOutlineRight className="rightDateBtn"/> */}
+                    <input
+                        type="range"
+                        className="custom-range"
+                        id="customRange1"
+                        style={{ width: "600px" }}
+                        min={0}
+                        max={24}
+                        value={range}
+                        onChange={(e) => {
+                            setRange(e.target.value);
+                            console.log(e.target.value);
+                            getHourStats(e.target.value);
+                            getGMTTime(slider[e.target.value].dt)
+                        }}
+                    />
+                </div>
+            </div>
+            {/* <div id="shortWeatherInfo" className="shortWeatherInfo">
             <div className="dailyAvg"></div>
             <div className="hourlyAvg">
                 <div className="tdDisplay">dt: {formattedHour}</div>
@@ -87,30 +87,38 @@ function DailyPage({ slider, dailyInfo, sunImg, Col, Row, Container }) {
                 <div className="hrWeather">UV: {currentHr.uvi}</div>
                 <div className="hrWeather">pressure: {currentHr.pressure}</div>
                 <div className="hrWeather">visibility: {currentHr.visibility}</div>
-            </div>
+            </div> */}
             <Container id="weatherSection">
                 <img src={sunImg} />
-                <div id="shortWeatherInfo" className="shortWeatherInfo">
-                    <div className="hourlyAvg">
-                        <Row id="temp">
-                            <div className="hrTemp">temp: {Math.round(currentHr.temp)}</div>
+                {/* <div id="shortWeatherInfo" className="shortWeatherInfo"> */}
+                    {/* <div className="hourlyAvg"> */}
+                        <Row id="shortWeatherInfo" className="shortWeatherInfo">
+                            <Row >
+                                <Col id="tempSpaceInfo" lg="4.5" md="4">
+                                    <Row className="hrTemp">{Math.round(currentHr.temp)}</Row>
+                                </Col>
+                                <Col id="topRightInfo" lg="2" md="2" >
+                                    <Row className="hrHumidity">humidity: {currentHr.humidity}%</Row>
+                                    <Row className="hrFeelLike">feels like: {Math.round(currentHr.feels_like)}</Row>
+                                    <Row className="hrWeather">Chance of Rain: {currentHr.pop}</Row>
+                                    <Row className="hrWeather">UV: {currentHr.uvi}</Row>
+                                </Col>
+                            </Row>
+                            <Row id="bottomInfo" lg="2" md="2">
+                                <Row className="tdDisplay">dt: {currentHr.dt}</Row>
+                                <Row className="hrClouds">cloudiness: {currentHr.clouds}%</Row>
+                                <Row className="hrWeather">weather conditions: {slider[testHr].weather[0].main}</Row>
+                                <Row className="hrWeather">Winds Speed: {currentHr.wind_speed}</Row>
+                                <Row className="hrWeather">pressure: {currentHr.pressure}</Row>
+                                <Row className="hrWeather">visibility: {currentHr.visibility}</Row>
+                            </Row>
                         </Row>
-                        <Row id="weatherTopRight">
-                            <div className="hrHumidity">humidity: {currentHr.humidity}%</div>
-                            <div className="hrFeelLike">feels like: {Math.round(currentHr.feels_like)}</div>
-                            <div className="hrWeather">Chance of Rain: {currentHr.pop}</div>
-                        </Row>
-                        <div className="tdDisplay">dt: {currentHr.dt}</div>
-                        <div className="hrClouds">cloudiness: {currentHr.clouds}%</div>
-                        <div className="hrWeather">weather conditions: {slider[testHr].weather[0].main}</div>
-                        <div className="hrWeather">Winds Speed: {currentHr.wind_speed}</div>
-                        <div className="hrWeather">UV: {currentHr.uvi}</div>
-                        <div className="hrWeather">pressure: {currentHr.pressure}</div>
-                        <div className="hrWeather">visibility: {currentHr.visibility}</div>
-                    </div>
-                </div>
+
+
+                    {/* </div> */}
+                {/* </div> */}
             </Container>
-            </div>
+            {/* </div> */}
         </>
     )
 }
