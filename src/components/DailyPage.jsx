2ae51d1e2144dcee3b4e0972f2ab447269ea0b40
Jadyn Calhoun <jaycal2013@gmail.com>
2021-05-17 15:27:40 -0700

    working on styles still- changing around grid

--- src/components/DailyPage.jsx
+++ src/components/DailyPage.jsx
@@ -15,10 +15,10 @@ function DailyPage({ slider, dailyInfo, Col, Row, Container }) {
     const [initialStuff, setInitialStuff] = useState(true);
     const [dayOrNight, setdayOrNight] = useState(false);
 
-    if(initialStuff == true){
+    if (initialStuff == true) {
         getHourStats(0);
         setInitialStuff(false);
-        getGMTTime(slider[Math.round(Number(0)+3)].dt)
+        getGMTTime(slider[Math.round(Number(0) + 3)].dt)
     }
 
     const [switchingTimes, setSwitchingTimes] = useState([]);
@@ -53,90 +53,90 @@ function DailyPage({ slider, dailyInfo, Col, Row, Container }) {
             return str;
         }
         let roundedSunrise = roundTimes(sunrise);
-        let roundedSunset = Number(roundTimes(sunset))+12;
+        let roundedSunset = Number(roundTimes(sunset)) + 12;
         let darkRange = [];
         let dayRange = [];
 
-        for(let i = 1; i<=24; i++){
-            if(i >= roundedSunrise && i < roundedSunset){
+        for (let i = 1; i <= 24; i++) {
+            if (i >= roundedSunrise && i < roundedSunset) {
                 dayRange.push(i);
-            }else{
+            } else {
                 darkRange.push(i);
             }
         }
 
-        let numBefore = Number(switchingTimes[Number(switchingTimes.length) -1]);
+        let numBefore = Number(switchingTimes[Number(switchingTimes.length) - 1]);
 
-        if(switchingTimes.length > 1){
+        if (switchingTimes.length > 1) {
             let firstRange = false;
             let secondRange = false;
-            for(let num = 0; num < dayRange.length; num++){
-                if(Number(numBefore) == Number(dayRange[num])){
-                firstRange = true;
+            for (let num = 0; num < dayRange.length; num++) {
+                if (Number(numBefore) == Number(dayRange[num])) {
+                    firstRange = true;
                 }
-                if(Number(curTime) == Number(dayRange[num])){
-                secondRange = true;
+                if (Number(curTime) == Number(dayRange[num])) {
+                    secondRange = true;
                 }
             }
-            if(firstRange == true && secondRange == false){
-            console.log(`it went from day to night`);
-            setAnimation("dayToNight");
-            }else if(firstRange == true && secondRange == true){
-            console.log("NO CHANGE DAY");
-            setAnimation("none");
-            }else if(firstRange == false && secondRange == true){
-            console.log("it went from night to day")
-            setAnimation("nightToDay");
-            }else{
-            console.log("NO CHANGE NIGHT")
-            setAnimation("none");
+            if (firstRange == true && secondRange == false) {
+                console.log(`it went from day to night`);
+                setAnimation("dayToNight");
+            } else if (firstRange == true && secondRange == true) {
+                console.log("NO CHANGE DAY");
+                setAnimation("none");
+            } else if (firstRange == false && secondRange == true) {
+                console.log("it went from night to day")
+                setAnimation("nightToDay");
+            } else {
+                console.log("NO CHANGE NIGHT")
+                setAnimation("none");
             }
         }
-        
-        
+
+
     }
     return (
         <>
-        <Container id="weatherSection">
-            <Row id="leftSide">
-        <div id="weatherAnimation" className="weatherAnimation"> 
-        </div>
-        <div id="weatherSlider" className="weatherSlider"> 
-            <div className="my-5">
-                <label htmlFor="customRange1">May 3 2021</label>
-                <div className="hourDisplay">{formattedHour}</div>
-                <input 
-                type="range" 
-                className="custom-range" 
-                id="customRange1" 
-                style={{width: "600px"}} 
-                min={0} 
-                max={23} 
-                value={range}
-                onChange={(e) => {
-                    setRange(Math.round(e.target.value));
-                    getHourStats(Math.round(e.target.value));
-                    getGMTTime(slider[Math.round(Number(e.target.value)+3)].dt);
-                    setSwitchingTimes([...switchingTimes, e.target.value]);
-                    sunChecker(e.target.value);
-                    if(e.target.value >= 6 && e.target.value < 20){
-                        setdayOrNight(true);
-                    }else{
-                        setdayOrNight(false);
+            <Container id="weatherSection">
+                <Row id="leftSide">
+                    <div id="weatherAnimation" className="weatherAnimation">
+                    </div>
+                    <div id="weatherSlider" className="weatherSlider">
+                        <div className="my-5">
+                            <label htmlFor="customRange1">May 3 2021</label>
+                            <div className="hourDisplay">{formattedHour}</div>
+                            <input
+                                type="range"
+                                className="custom-range"
+                                id="customRange1"
+                                style={{ width: "600px" }}
+                                min={0}
+                                max={23}
+                                value={range}
+                                onChange={(e) => {
+                                    setRange(Math.round(e.target.value));
+                                    getHourStats(Math.round(e.target.value));
+                                    getGMTTime(slider[Math.round(Number(e.target.value) + 3)].dt);
+                                    setSwitchingTimes([...switchingTimes, e.target.value]);
+                                    sunChecker(e.target.value);
+                                    if (e.target.value >= 6 && e.target.value < 20) {
+                                        setdayOrNight(true);
+                                    } else {
+                                        setdayOrNight(false);
 
+                                    }
+                                }}
+                            />
+                        </div>
+                    </div>
+                    {dayOrNight ?
+                        (<Row>
+                            <Col id="img" src={sunImg} style={{ backgroundImage: `url('${sunImg}')` }}> </Col>
+                        </Row>) :
+                        (<Row>
+                            <Col id="img" src={cloud1} alt="moon Image" style={{ backgroundImage: `url('${cloud1}')` }}> </Col>
+                        </Row>)
                     }
-                }} 
-                />
-            </div>
-        </div>
-                {dayOrNight ?
-                    (<Row>
-                        <Col id="img" src={sunImg} style={{backgroundImage: `url('${sunImg}')`}}> </Col>
-                    </Row>) :
-                    (<Row>
-                        <Col id="img" src={cloud1} alt="moon Image" style={{backgroundImage: `url('${cloud1}')`}}> </Col>
-                    </Row>)
-                }
                 </Row>
                 <Row id="shortWeatherInfo" className="shortWeatherInfo" >
                     <Row id="top">
@@ -144,21 +144,27 @@ function DailyPage({ slider, dailyInfo, Col, Row, Container }) {
                             <Row className="hrTemp">{Math.round(currentHr.temp)}</Row>
                         </Col>
                         <Col id="topRightInfo" lg="6" md="2" >
-                            
+
                             <Row className="hrWeather">Date: {currentHr.dt}</Row>
                             <Row className="hrWeather">Feels Like: {Math.round(currentHr.feels_like)}</Row>
                             <Row className="hrWeather">Chance of Rain: {currentHr.pop}</Row>
                             <Row className="hrWeather">UV: {currentHr.uvi}</Row>
-                            
+
                         </Col>
                     </Row>
-                    <Row id="bottomInfo" lg="3" >
-                        <Row className="hrWeather"><span className="title">Humidity:</span><span className="titleText">{currentHr.humidity}%</span></Row>
-                        <Row className="hrWeather"><span className="title">Cloudiness:</span><span className="titleText">{currentHr.clouds}%</span></Row>
-                        <Row className="hrWeather"><span className="title">Weather:</span><span className="titleText">{slider[testHr].weather[0].main}</span></Row>
-                        <Row className="hrWeather"><span className="title">Wind Speed:</span><span className="titleText">{currentHr.wind_speed}</span></Row>
-                        <Row className="hrWeather"><span className="title">Pressure:</span><span className="titleText">{currentHr.pressure}</span></Row>
-                        <Row className="hrWeather"><span className="title">Visibility:</span><span className="titleText">{currentHr.visibility}</span></Row>
+                    <Row id="bottomInfo">
+                        <Col className="bottomCols">
+                            <div lg="4" className="hrWeather flex-column"><div className="title">Humidity:</div><div className="titleText">{currentHr.humidity}%</div></div>
+                            <div lg="4" className="hrWeather flex-column"><div className="title">Cloudiness:</div><div className="titleText">{currentHr.clouds}%</div></div>
+                        </Col>
+                        <Col className="bottomCols">
+                            <div lg="4" className="hrWeather flex-column"><div className="title">Weather:</div><div className="titleText">{slider[testHr].weather[0].main}</div></div>
+                            <div lg="4" className="hrWeather flex-column"><div className="title">Wind Speed:</div><div className="titleText">{currentHr.wind_speed}</div></div>
+                        </Col>
+                        <Col className="bottomCols">
+                            <div lg="4" className="hrWeather flex-column"><div className="title">Pressure:</div><div className="titleText">{currentHr.pressure}</div></div>
+                            <div lg="4" className="hrWeather flex-column"><div className="title">Visibility:</div><div className="titleText">{currentHr.visibility}</div></div>
+                        </Col>
                     </Row>
                 </Row>
 
